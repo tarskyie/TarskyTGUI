6ff7c950b599f364ea9e6972277c576e6f765dc2
tarskyie <tarskyivan98@gmail.com>
2023-10-11 07:55:34 +0900

    seed fix

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class LlamaGUI(QWidget):
         if seed == "-1" or seed == "":
             seed = random.randint(1, 10000000)
         print("SEED: " + str(seed))
-        self.model = Llama(model_path = modelpath, n_ctx=4096, seed=seed)
+        self.model = Llama(model_path = modelpath, n_ctx=4096, seed=int(seed))
 
     def initUI(self):
         # Set dark theme colors
