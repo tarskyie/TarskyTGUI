bfc82fd205c556b9f123f697ab36893da209a197
tarskyie <tarskyivan98@gmail.com>
2023-10-11 11:22:26 +0900

    oops

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class LlamaGUI(QWidget):
         if seed == "-1" or seed == "":
             seed = random.randint(1, 10000000)
         if ctx == "":
-            ctx = 1
+            ctx = 4096
         print("SEED: " + str(seed))
         self.model = Llama(model_path = modelpath, n_ctx=abs(int(ctx)), seed=int(seed))
 
